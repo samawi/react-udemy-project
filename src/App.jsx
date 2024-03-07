98ef3c148e1b16abbceeaef21c7283f5b30c4275
Samawi Samadikun <samawi.samadikun@gmail.com>
2024-03-07 18:06:53 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Greet from './Greet'
+
+function App() {
+
+  return (
+    <>
+    <h1>Hello World!</h1>
+    <Greet />
+    </>
+  )
+}
+
+export default App
---
+++ src/Greet.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Greet.css'
+
+function Greet() {
+  return (
+    <h2 className='Greet'>Greet</h2>
+  )
+}
+
+export default Greet
